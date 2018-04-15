0659b12cec8ee5bc73fd484c11032af8fa9a1721
Maksim Degtyarev <brokkoli08@gmail.com>
2018-04-15 21:19:22 +0300

    prepare for filling every cell

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -5,7 +5,15 @@ interface Props {
   width: number;
   height: number;
 }
-const _cell: StyledFunction<Props & React.HTMLProps<HTMLDivElement>> = SC.div;
+interface State {
+  cells: Array<Array<boolean>>;
+}
+interface CellProps {
+  width: number;
+  height: number;
+  active: boolean;
+}
+const _cell: StyledFunction<CellProps & React.HTMLProps<HTMLDivElement>> = SC.div;
 const _row: StyledFunction<React.HTMLProps<HTMLDivElement>> = SC.div;
 const Cell = _cell`
   width: ${p => p.width}px;
@@ -13,22 +21,26 @@ const Cell = _cell`
   display: inline-block;
   border: 1px solid #000;
   box-sizing: border-box;
+  background-color: ${p => p.active ? 'black' : 'transparent'};
 `;
 const Row = _row`
   font-size: 0px;
   box-sizing: border-box;
 `;
 
-export default class extends React.Component {
+export default class extends React.Component<Props, State> {
   cellWidth: number = 10;
   cellHeight: number = 10;
-  size: number = 10;
+  size: number = 50;
+  state: State;
   
   constructor(props: Props) {
     super(props);
-    this.size = 100;
     this.cellWidth = window.innerWidth / this.size;
     this.cellHeight = window.innerHeight / this.size;
+    this.state = {
+      cells: Array(this.size).fill(Array(this.size).fill(false)),
+    };
   }
   render() {
     const count = Array(this.size).fill(1).map((i, j) => i + j);
@@ -37,7 +49,7 @@ export default class extends React.Component {
         <Row key={cellH}>
           {
             count.map(cellV => {
-              return <Cell key={`${cellH}_${cellV}`} width={this.cellWidth} height={this.cellHeight} />;
+              return <Cell key={`${cellH}_${cellV}`} width={this.cellWidth} height={this.cellHeight} active={false} />;
             })
           }
         </Row>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/background';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
